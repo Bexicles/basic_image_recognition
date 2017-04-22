7dd2c5941347b64946762dc1da128f040c2b16a1
becci.denny <bexicles@gmail.com>
2017-04-22 13:48:32 +0100

    Added a multi-layer fully connected neural net. This works, but I don't think it's performing well, i.e. isn't learning.

---
+++ multi_layer_neural_net.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+import control_panel
+import data_setup
+
+n = control_panel.solutions_number  # number of solution classes
+p = control_panel.image_pixels  # number of input pixels
+a = control_panel.alpha # learning rate
+N = data_setup.t1   # number of 'images' in training data set
+
+n_hidden_1 = 256    # number of features in first layer
+n_hidden_2 = 256    # number of features in second layer
+n_hidden_3 = 256    # number of features in third layer
+
+X_train = data_setup.Train_X  # training matrix of x values
+Y_train = data_setup.Hot_train_Y  # training vector of y labels (one hot format)
+X_test = data_setup.Test_X  # test matrix of x values
+Y_test = data_setup.Hot_test_Y  # test vector of y labels (one hot format)
+
+x = tf.placeholder(tf.float32, [None, p])   # placeholder for the input data, x
+y_ = tf.placeholder(tf.float32, [None, n])  # placeholder for the actual values of y, in training data
+
+# Function to build model
+def multilayer_perceptron(x, weights, biases):
+    layer_1 = tf.add(tf.matmul(x, weights['h1']),biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)   # RELU activation
+
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']),biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)   # RELU activation
+
+    layer_3 = tf.add(tf.matmul(layer_2, weights['h3']),biases['b3'])
+    layer_3 = tf.nn.relu(layer_3)   # RELU activation
+
+    output_layer = tf.matmul(layer_3, weights['h4']) + biases['b4'] # output layer, linear activation
+    return output_layer
+
+
+# Dictionaries to store layers' weights & biases
+weights = {
+    'h1': tf.Variable(tf.random_normal([p, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3])),
+    'h4': tf.Variable(tf.random_normal([n_hidden_3, n])),
+}
+
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'b3': tf.Variable(tf.random_normal([n_hidden_3])),
+    'b4': tf.Variable(tf.random_normal([n])),
+
+}
+
+
+# Model
+y = multilayer_perceptron(x, weights, biases)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))   # cross entropy of difference between calculated answer and actual answer
+train_step = tf.train.GradientDescentOptimizer(a).minimize(cross_entropy)   # performs Gradient Descent to minimise cross entropy
+
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+
+sess.run(train_step, feed_dict={x: X_train, y_: Y_train})
+
+# Test model
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+
+# Calculate accuracy
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: X_test, y_: Y_test}))
