5f191f9caf7fb185769b9355f843bc08eb42bb26
becci.denny <bexicles@gmail.com>
2017-05-14 21:50:40 +0100

    Changed learning rate to try to improve accuracy

--- control_panel.py
+++ control_panel.py
@@ -3,6 +3,6 @@ image_pixels = image_size[0] * image_size[1]
 
 training_split = 0.75   # proportion of original data set, set aside for training the model
 
-alpha = 0.00001 # learning rate
+alpha = 0.000015 # learning rate
 solutions_number = 4   # no. of possible answers is two (me, dad, joan or peet; 0, 1, 2 or 3)
 batch_size = 50
